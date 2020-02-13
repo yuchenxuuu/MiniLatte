8c039b7358a8428c301d72c102bd0ae34b00c6a0
yuchenxuuu <yuchenxu990701@gmail.com>
2020-02-13 11:35:25 -0500

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :instructors
   resources :courses
   resources :subjects
+  
   root :to => "page#home"
+
   get  'course_list',    to: 'courses#index'
   get  'subject_list',    to: 'subjects#index'
   get  'instructor_list',    to: 'instructors#index'
