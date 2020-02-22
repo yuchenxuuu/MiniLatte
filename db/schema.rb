5e7cd0e6cff55e6f2b60e4e285f4868f3e6a8e72
yuchenxuuu <yuchenxu990701@gmail.com>
2020-02-22 15:11:33 -0500

    user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_191051) do
+ActiveRecord::Schema.define(version: 2020_02_22_195040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2020_02_20_191051) do
     t.float "credits"
     t.boolean "independentstudy"
     t.string "requirements"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -56,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_191051) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -16,6 +16,7 @@ class UsersTest < ApplicationSystemTestCase
 
     fill_in "Email", with: @user.email
     fill_in "Name", with: @user.name
+    fill_in "Password", with: @user.password
     click_on "Create User"
 
     assert_text "User was successfully created"
@@ -28,6 +29,7 @@ class UsersTest < ApplicationSystemTestCase
 
     fill_in "Email", with: @user.email
     fill_in "Name", with: @user.name
+    fill_in "Password", with: @user.password
     click_on "Update User"
 
     assert_text "User was successfully updated"
